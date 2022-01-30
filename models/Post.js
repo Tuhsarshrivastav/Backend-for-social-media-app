1f03cf0a7dd84ecb2f2ae83e972830df254751c5
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-30 18:22:53 +0530

    added post model

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  caption: String,
+  imageUrl: {
+    public_id: String,
+    url: String,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  like: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+module.exports = mongoose.model("Post", postSchema);
---
+++ models/User.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({});
+
+module.exports = mongoose.model("User", userSchema);
