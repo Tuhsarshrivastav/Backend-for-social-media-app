c08d3f04049fe4d95474860a6c19a3ecc75e3d45
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-30 18:28:56 +0530

    added user model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,42 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [, "Please enter a name"],
+  },
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter an password"],
+    minlength: [6, "Password must be at least 6 characters"],
+    select: false,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+});
 
 module.exports = mongoose.model("User", userSchema);
