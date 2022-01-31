933f1c489eca6ca362d0bc23159b1a1cf57cd2ce
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-31 15:35:35 +0530

    added middleware buildin

--- app.js
+++ app.js
@@ -4,4 +4,14 @@ const app = express();
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+const post = require("./routes/postRoute");
+
+// Using routes
+app.use("/api/v1/", post);
 module.exports = app;
