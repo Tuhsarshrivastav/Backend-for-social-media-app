a76f4623d75a85dde2e0632e3a902a12c7767b77
Tuhar Shrivastav <ts3657@gmail.com>
2022-02-01 18:32:58 +0530

    delete post api

--- controllers/postController.js
+++ controllers/postController.js
@@ -56,3 +56,32 @@ exports.likeAndunlikePost = async (req, res) => {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+    if (post.owner.toString() !== req.user._id.toString()) {
+      return res.status(401).json({
+        success: false,
+        message: "You are not authorized to delete this post",
+      });
+    }
+    await post.remove();
+    const user = await User.findById(req.user._id);
+    const index = user.posts.indexOf(req.params.id);
+    user.posts.splice(index, 1);
+    await user.save();
+    res.status(200).json({
+      success: true,
+      message: "Post deleted",
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
