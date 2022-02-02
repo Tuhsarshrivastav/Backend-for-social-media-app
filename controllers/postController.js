25a7e17f9310fb8c5d139c2113ca057d51bb3ce3
Tuhar Shrivastav <ts3657@gmail.com>
2022-02-02 16:11:16 +0530

    added follwing posts api

--- controllers/postController.js
+++ controllers/postController.js
@@ -85,3 +85,20 @@ exports.deletePost = async (req, res) => {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+exports.getPostOfFollowing = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    const posts = await Post.find({
+      owner: { $in: user.following },
+    });
+    res.status(200).json({
+      success: true,
+      posts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -4,6 +4,7 @@ const {
   createPost,
   likeAndunlikePost,
   deletePost,
+  getPostOfFollowing,
 } = require("../controllers/postController");
 const { isAuthenticated } = require("../middlewares/auth");
 router.route("/post/upload").post(isAuthenticated, createPost);
@@ -11,4 +12,5 @@ router
   .route("/post/:id")
   .get(isAuthenticated, likeAndunlikePost)
   .delete(isAuthenticated, deletePost);
+router.route("/posts").get(isAuthenticated, getPostOfFollowing);
 module.exports = router;
