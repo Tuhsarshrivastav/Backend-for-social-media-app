768f59f7274db92560c2c6f4101a8f56183a4599
Tuhar Shrivastav <ts3657@gmail.com>
2022-02-02 16:19:16 +0530

    addedlogout api

--- controllers/userController.js
+++ controllers/userController.js
@@ -75,6 +75,23 @@ exports.login = async (req, res) => {
   }
 };
 
+exports.logout = async (req, res) => {
+  try {
+    res
+      .status(200)
+      .cookie("token", null, { expires: new Date(Date.now()), httpOnly: true })
+      .json({
+        success: true,
+        message: "User logged out",
+      });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 exports.followUser = async (req, res) => {
   try {
     const userToFollow = await User.findById(req.params.id);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,9 +4,11 @@ const {
   register,
   login,
   followUser,
+  logout,
 } = require("../controllers/userController");
 const { isAuthenticated } = require("../middlewares/auth");
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/follow/:id").get(isAuthenticated, followUser);
+router.route("/logout").get(logout);
 module.exports = router;
