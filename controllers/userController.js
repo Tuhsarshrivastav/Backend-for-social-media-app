10b3a398ba61de26f024ecbf33938b574deef8ad
Tuhar Shrivastav <ts3657@gmail.com>
2022-02-02 18:43:57 +0530

    update profile api

--- controllers/userController.js
+++ controllers/userController.js
@@ -160,3 +160,26 @@ exports.updatePassword = async (req, res) => {
     });
   }
 };
+
+exports.updateProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    const { name, email } = req.body;
+    if (name) {
+      user.name = name;
+    }
+    if (email) {
+      user.email = email;
+    }
+    await user.save();
+    res.status(200).json({
+      success: true,
+      message: "Profile updated",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,10 +5,14 @@ const {
   login,
   followUser,
   logout,
+  updatePassword,
+  updateProfile,
 } = require("../controllers/userController");
 const { isAuthenticated } = require("../middlewares/auth");
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/follow/:id").get(isAuthenticated, followUser);
 router.route("/logout").get(logout);
+router.route("/update/password").put(isAuthenticated, updatePassword);
+router.route("/update/profile").get(isAuthenticated, updateProfile);
 module.exports = router;
